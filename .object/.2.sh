d6f73c6dfe3715b1a8f88a75b6df86b331e49637
〄 ＤＡＲＫ－Ｄ699 ايڪـͬــͤــᷜــͨــͣــͪـي <tbnadia111@gmail.com>
2025-06-30 22:31:06 +0530

    Update .2.sh

--- .object/.2.sh
+++ .object/.2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 PUT(){ echo -en "\033[${1};${2}H";}
-clear;toilet -t -f mono12  "H4Ck3R" --gay -F border
+clear;toilet -t -f mono12  "DARK-D" --gay -F border
 echo ""
 PUT 11 40
-echo -e "\e[92mBy\e[1;93m RajAryan\e[1;95m /\e[1;96m H4Ck3R0 "
+echo -e "\e[92mBy\e[1;93m RajAryan\e[1;95m /\e[1;96m DARK-D "
 PUT 12 3
 echo
 echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[100;92myoutube.com/h4ck3r_raj\e[0m"    
